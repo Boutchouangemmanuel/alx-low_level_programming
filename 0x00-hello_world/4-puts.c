2e6edb78491a7f20548ca3903c9c026ffef7a9bc
Boutchouangemmanuel <emmanuelgireh@gmail.com>
2022-04-11 04:45:38 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * 
+ * Returnl always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzule");
+return (0);
+}
